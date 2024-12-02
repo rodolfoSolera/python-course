c41a2affaa249eb2b64a83a0ea94e1b8cf5646e4
Rodolfo P. Solera <rod.solera@icloud.com>
2024-12-01 21:52:15 -0300

    Inclusão das duas primeiras aulas realizadas do curso de Python.

---
+++ aulas/aula1.py
@@ -0,0 +1,12 @@
+"""
+DocString - O interpretador do Python Lê essas linhas com três aspas.
+Usado para escrever notas.  
+"""
+'''
+DocString com asas simples também interpretado pelo Python e usado para escrever notas.
+
+'''
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(1+1)
